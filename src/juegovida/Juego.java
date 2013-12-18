c3f359e7521329b6f512094fae7c8686a2691b4e
Chema Valera Reales <chemaclass@outlook.es>
2013-12-18 16:23:13 +0100

    Añadiendo licencia GNU

--- src/juegovida/Juego.java
+++ src/juegovida/Juego.java
@@ -1,3 +1,21 @@
+/*
+ * Conversor -  Copyright (C) 2013 
+ * José María Valera Reales <chemaclass@outlook.es> Twitter: @Chemaclass
+ * http://www.chemaclass.com
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package juegovida;
 
 import java.util.ArrayList;
@@ -6,7 +24,8 @@ import javax.swing.JTextArea;
 
 /**
  *
- * @author chema
+ * @author Chemaclass
+ * @version 1.0
  */
 public class Juego extends Thread {
 
